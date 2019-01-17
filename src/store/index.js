8bb15ce75bfede622507ed2680f3105f31232498
ziv-zhao <ziv.zhao@perficient.com>
2019-01-17 16:05:58 +0800

    update vuex hot-reloading

--- src/store/index.js
+++ src/store/index.js
@@ -5,9 +5,27 @@ import logger from "./plugins/logger";
 
 Vue.use(Vuex);
 
-export default new Vuex.Store({
+const store = new Vuex.Store({
+  strict: process.env.NODE_ENV !== "production",
   modules: {
     postModule
   },
   plugins: process.env.NODE_ENV !== "production" ? [logger] : []
 });
+
+if (module.hot) {
+  // 使 action 和 mutation 成为可热重载模块
+  module.hot.accept(["./modules/postModule"], () => {
+    // 获取更新后的模块
+    // 因为 babel 6 的模块编译格式问题，这里需要加上 `.default`
+    const newPostModule = require("./modules/postModule").default;
+    // 加载新模块
+    store.hotUpdate({
+      modules: {
+        postModule: newPostModule
+      }
+    });
+  });
+}
+
+export default store;
